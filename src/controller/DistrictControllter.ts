591f421b1a247f05b0dccc3b12e020790799b65b
MrRabbit <oujian@citycloud.com.cn>
2022-12-06 10:30:07 +0800

    feat: update

--- src/controller/DistrictControllter.ts
+++ src/controller/DistrictControllter.ts
@@ -4,7 +4,7 @@ import { APIPrefix } from "./../config"
 import DistrictServe from "../services/DistrictService"
 import { IDistrictParams } from "./../types/District"
 const district = new Router<Koa.DefaultState, Koa.DefaultContext>({ prefix: `${APIPrefix}/v1/district` })
-const JSON = require("./../sys_district.json")
+// const JSON = require("./../sys_district.json")
 district.post('/add', async (ctx) => {
   // JSON.RECORDS.forEach(async(item:unknown) => {
   await DistrictServe.addDistrict(ctx.request.body as IDistrictParams)
